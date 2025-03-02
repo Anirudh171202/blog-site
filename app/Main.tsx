179867259bbbf78a2c1df89d8f1264bd07c720c9
Anirudh Sathiya <anirudh.sathiya@gmail.com>
2025-03-02 12:39:21 -0500

    added analytics v2

--- app/Main.tsx
+++ app/Main.tsx
@@ -3,7 +3,7 @@ import Tag from '@/components/Tag'
 import siteMetadata from '@/data/siteMetadata'
 import { formatDate } from 'pliny/utils/formatDate'
 import NewsletterForm from 'pliny/ui/NewsletterForm'
-import { Analytics } from "@vercel/analytics/react"
+import { Analytics } from '@vercel/analytics/react'
 
 const MAX_DISPLAY = 5
 
@@ -15,6 +15,7 @@ export default function Home({ posts }) {
           <h1 className="text-3xl font-extrabold leading-9 tracking-tight text-gray-900 dark:text-gray-100 sm:text-4xl sm:leading-10 md:text-6xl md:leading-14">
             Latest
           </h1>
+          <Analytics />
           <p className="text-lg leading-7 text-gray-500 dark:text-gray-400">
             {siteMetadata.description}
           </p>
@@ -65,7 +66,6 @@ export default function Home({ posts }) {
                       </div>
                     </div>
                   </div>
-                  <Analytics/>
                 </article>
               </li>
             )
