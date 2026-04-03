62da40ca08d5d73c816dbfd7908908bb35202f9c
Anirudh Sathiya <anirudh.sathiya@gmail.com>
2026-04-03 18:49:56 -0400

    uploading draft inference blog

--- next.config.js
+++ next.config.js
@@ -10,7 +10,7 @@ const ContentSecurityPolicy = `
   script-src 'self' 'unsafe-eval' 'unsafe-inline' giscus.app vitals.vercel-insights.com vitals.vercel-analytics.com d3js.org;
   style-src 'self' 'unsafe-inline';
   img-src * blob: data:;
-  media-src *.s3.amazonaws.com;
+  media-src *.s3.amazonaws.com storage.googleapis.com;
   connect-src * vitals.vercel-insights.com vitals.vercel-analytics.com;
   font-src 'self';
   frame-src *;
